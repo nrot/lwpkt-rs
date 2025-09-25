da6fc58ef6d8f80885984692691f61deddf9efb2
nrot <nrot13@gmail.com>
2025-09-25 17:04:38 +0300

    First try

---
+++ build.rs
@@ -0,0 +1,120 @@
+use std::env;
+use std::fs::File;
+use std::io::{Read, Write};
+use std::path::{Path, PathBuf};
+
+#[derive(Default)]
+struct LwPktOptions {
+    max_data_len: Option<usize>,
+}
+
+impl LwPktOptions {
+    const END_FILE: &str = "#endif /* LWPKT_OPTS_HDR_H */";
+
+    fn generate(self, path: &Path) {
+        let mut origin_f = File::open("lwpkt_opts_template.h").unwrap();
+
+        let mut f = File::create(path).unwrap();
+
+        let mut b = vec![];
+        origin_f.read_to_end(&mut b).unwrap();
+
+        f.write_all(&b).unwrap();
+
+        drop(b);
+        drop(origin_f);
+
+        if let Some(v) = self.max_data_len {
+            f.write_all(format!("\n#define LWPKT_CFG_MAX_DATA_LEN {v}").as_bytes())
+                .unwrap();
+        }
+
+        f.write_all(Self::END_FILE.as_bytes()).unwrap();
+        f.flush().unwrap();
+    }
+}
+
+fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+
+    // Tell cargo to look for shared libraries in the specified directory
+    // println!("cargo:rustc-link-search=/path/to/lib");
+
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    // println!("cargo:rustc-link-lib=bz2");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+
+    let mut options = LwPktOptions::default();
+
+    if let Some(v) = std::env::var_os("LWPKT_CFG_MAX_DATA_LEN") {
+        options.max_data_len = Some(v.to_str().unwrap().parse::<usize>().unwrap());
+    }
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let out_lwpkt = out_path.join("lwpkt");
+    let out_lwrb = out_path.join("lwrb");
+
+    let _ = std::fs::create_dir(&out_lwpkt);
+    let _ = std::fs::create_dir(&out_lwrb);
+
+    let options_path = out_lwpkt.join("lwpkt_opts.h");
+    options.generate(&options_path);
+
+    let opt_file = out_lwpkt.join("lwpkt_opt.h");
+    std::fs::copy("src/lwpkt/lwpkt/src/include/lwpkt/lwpkt_opt.h", &opt_file).unwrap();
+
+    // Second copy for link lwpkt.c
+    let lwpk_h = out_lwpkt.join("lwpkt.h");
+    std::fs::copy("src/lwpkt/lwpkt/src/include/lwpkt/lwpkt.h", &lwpk_h).unwrap();
+
+    let lwpk_h = out_path.join("lwpkt.h");
+    std::fs::copy("src/lwpkt/lwpkt/src/include/lwpkt/lwpkt.h", &lwpk_h).unwrap();
+    
+
+    let lwpk_c = out_path.join("lwpkt.c");
+    std::fs::copy("src/lwpkt/lwpkt/src/lwpkt/lwpkt.c", &lwpk_c).unwrap();
+
+    let lwrb_h = out_lwrb.join("lwrb.h");
+    std::fs::copy("src/lwpkt/libs/lwrb/src/include/lwrb/lwrb.h", &lwrb_h).unwrap();
+
+    let lwrb_c: PathBuf = out_path.join("lwrb.c");
+    std::fs::copy("src/lwpkt/libs/lwrb/src/lwrb/lwrb.c",    &lwrb_c).unwrap();
+
+    // let header = &[out_path];
+    let c_source = &[lwrb_c, lwpk_c];
+
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .clang_arg("-DLWRB_DISABLE_ATOMIC")
+        .header(lwrb_h.into_os_string().into_string().unwrap())
+        .header(options_path.into_os_string().into_string().unwrap())
+        .header(opt_file.into_os_string().into_string().unwrap())
+        .header(lwpk_h.into_os_string().into_string().unwrap())
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .derive_default(true)
+        .default_enum_style(bindgen::EnumVariation::ModuleConsts)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+
+    let mut builder = cc::Build::new();
+    builder.files(c_source);
+    builder.include(&out_path);
+    builder.compile("lwpkt");
+
+    bindings
+        .write_to_file(out_path.join("lwpkt.rs"))
+        .expect("Couldn't write bindings!");
+
+    println!("cargo:include={}", out_path.to_str().unwrap());
+}
---
+++ src/lib.rs
@@ -0,0 +1,139 @@
+mod ffi;
+
+pub struct LwRb {
+    lwrb: ffi::lwrb,
+    buffer: Vec<u8>,
+}
+
+impl LwRb {
+    pub fn new(size: usize) -> Self {
+        let mut lwrb = ffi::lwrb::default();
+
+        let mut buffer = vec![0u8; size];
+
+        let res = unsafe {
+            ffi::lwrb_init(
+                &mut lwrb as *mut _,
+                buffer.as_mut_ptr() as *mut _,
+                buffer.len(),
+            )
+        };
+
+        debug_assert_eq!(res, 1);
+
+        Self { lwrb, buffer }
+    }
+}
+
+pub enum Error {
+    ERR = 0x1,
+    INPROG,
+    VALID,
+    ERRCRC,
+    ERRSTOP,
+    WAITDATA,
+    ERRMEM,
+}
+
+impl From<ffi::lwpktr_t::Type> for Error {
+    fn from(value: ffi::lwpktr_t::Type) -> Self {
+        match value {
+            ffi::lwpktr_t::lwpktERR => Self::ERR,
+            ffi::lwpktr_t::lwpktINPROG => Self::INPROG,
+            ffi::lwpktr_t::lwpktVALID => Self::VALID,
+            ffi::lwpktr_t::lwpktERRCRC => Self::ERRCRC,
+            ffi::lwpktr_t::lwpktERRSTOP => Self::ERRSTOP,
+            ffi::lwpktr_t::lwpktWAITDATA => Self::WAITDATA,
+            ffi::lwpktr_t::lwpktERRMEM => Self::ERRMEM,
+            e => {
+                panic!("Unknow type error: {e}")
+            }
+        }
+    }
+}
+
+fn check_result(res: u32) -> Result<(), Error> {
+    if res == ffi::lwpktr_t::lwpktOK {
+        Ok(())
+    } else {
+        Err(res.into())
+    }
+}
+
+pub struct LwPkt {
+    lwpkt: ffi::lwpkt,
+    read_buffer: LwRb,
+    write_buffer: LwRb,
+}
+
+impl LwPkt {
+    pub const MAX_PACKAGE_SIZE: u32 = ffi::LWPKT_CFG_MAX_DATA_LEN;
+
+    pub fn new(read_buffer: LwRb, write_buffer: LwRb) -> Result<Self, Error> {
+        let lwpkt = ffi::lwpkt::default();
+
+        let mut result = Self {
+            lwpkt,
+            read_buffer,
+            write_buffer,
+        };
+
+        let res = unsafe {
+            ffi::lwpkt_init(
+                &mut result.lwpkt as *mut _,
+                &mut result.write_buffer.lwrb as *mut _,
+                &mut result.read_buffer.lwrb as *mut _,
+            )
+        };
+        check_result(res)?;
+
+        Ok(result)
+    }
+
+    pub fn set_addres(&mut self, address: u8) -> Result<(), Error> {
+        let res = unsafe { ffi::lwpkt_set_addr(&mut self.lwpkt as *mut _, address) };
+
+        check_result(res)
+    }
+
+    pub fn write(&mut self, address: u8, command: u32, data: &[u8]) -> Result<(), Error> {
+        let res = unsafe {
+            ffi::lwpkt_write(
+                &mut self.lwpkt as *mut _,
+                address,
+                command,
+                data.as_ptr() as *mut _,
+                data.len(),
+            )
+        };
+
+        check_result(res)
+    }
+
+    pub fn read(&mut self) -> Result<(u32, &[u8]), Error> {
+        let res = unsafe { ffi::lwpkt_read(&mut self.lwpkt as *mut _) };
+
+        check_result(res)?;
+
+        Ok((self.get_cmd(), self.get_data()))
+    }
+
+    ///
+    /// /**
+    ///  * \brief           Get pointer to packet data
+    ///  * \param[in]       pkt: LwPKT instance
+    ///  * \return          Pointer to data
+    ///  */
+    /// #define lwpkt_get_data(pkt)      (void*)(((pkt) != NULL) ? ((pkt)->data) : NULL)
+    fn get_data(&self) -> &[u8] {
+        &self.lwpkt.data
+    }
+
+    fn get_cmd(&self) -> u32 {
+        self.lwpkt.m.cmd
+    }
+}
+
+pub fn test() {
+    
+}
